303018e86b5a01d9b951328d4254b5c8e33aaac2
Lucio Westmoreland <luciovw@gmail.com>
2020-11-10 17:40:50 -0800

    added second button approach

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,15 @@
 
 #include <JuceHeader.h>
 
+struct DualButton : public Component
+{
+    DualButton();
+    void resized() override;
+    
+private:
+    TextButton button1{"button1"}, button2{"button2"};
+};
+
 struct Widget : public Component
 {
     Widget(int i) : num(i) {}
@@ -98,6 +107,7 @@ private:
     //int counter = 0;
     MyComp comp;
     OwnedArrayComponent ownedArrayComp;
+    DualButton dualButton;
     //==============================================================================
     // Your private member variables go here...
 
